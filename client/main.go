5ca34f0eb08bc490714c48344de484f5629f27b0
iItachiUchiha <iyogeshsaini1008@gmail.com>
2023-08-20 10:45:00 +0530

    Initial Commit for grpc project

---
+++ client/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	pb "github.com/PraneGIT/gRPC-complete/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	names := &pb.NameList{
+		Names: []string{"praneki", "pranjal", "jha"},
+	}
+
+	// Call the remote method
+	// callSayHello(client)
+
+	// callSayHelloServerStreaming(client, names)
+
+	// callSayHelloClientStream(client, names)
+
+	callHelloBidirectionalStream(client, names)
+}
